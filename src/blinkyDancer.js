291803810ead29566c34a73c3fb5b4f6306ee034
Leighton <leighton.kaina@gmail.com>
2013-10-01 17:38:55 -0700

    blinky dancer now transforms and lines up when clicked.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,12 +1,23 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
+  this.className = 'blinkydancer';
   Dancer.call(this, top, left, timeBetweenSteps);
+  var red = 255;
+  var green = Math.floor(Math.random()*255);
+  var blue = Math.floor(Math.random()*255);
+  this.$node.css({'border': '10px solid rgb('+red+','+green+','+blue});
 };
 
-BlinkyDancer.prototype = new Dancer();
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 
 BlinkyDancer.prototype.step = function() {
-  // call the old version of step at the beginning of any call to this new version of step
   Dancer.prototype.step.call(this);
-  this.$node.toggle();
+  this.$node.toggle({'opacity': 1});
+  this.$node.animate({'width': 50, 'height':45, 'border-radius':20}, Math.random()*500 + 500);
 };
+
+BlinkyDancer.prototype.lineUp = function() {
+  var newTop = $(window).height() - 50;
+  Dancer.prototype.setPosition.call(this, newTop, this.left);
+};
+
