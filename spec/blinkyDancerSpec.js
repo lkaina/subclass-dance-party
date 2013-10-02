8af6eb18ee56e53a381740e547d541a48f24a353
Leighton <leighton.kaina@gmail.com>
2013-10-01 17:38:32 -0700

    added tests for other dancer subclasses.

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -31,3 +31,71 @@ describe("blinkyDancer", function() {
     });
   });
 });
+
+describe("blueDancer", function() {
+
+  var blueDancer;
+  var regularDancer;
+  var timeBetweenSteps = 100;
+  var clock;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    blueDancer = new BlueDancer(10, 20, timeBetweenSteps);
+  });
+
+  it("should have a jQuery $node object", function(){
+    expect(blueDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it("should have a step function that makes its node blink", function() {
+    sinon.spy(blueDancer.$node, 'toggle');
+    blueDancer.step();
+    expect(blueDancer.$node.toggle.called).to.be.true;
+  });
+
+  describe("dance", function(){
+    it("should call step at least once per second", function(){
+      sinon.spy(blueDancer, "step");
+      expect(blueDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps);
+      expect(blueDancer.step.callCount).to.be.equal(1);
+      clock.tick(timeBetweenSteps);
+      expect(blueDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
+
+describe("hippieDancer", function() {
+
+  var hippieDancer;
+  var regularDancer;
+  var timeBetweenSteps = 100;
+  var clock;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    hippieDancer = new HippieDancer(10, 20, timeBetweenSteps);
+  });
+
+  it("should have a jQuery $node object", function(){
+    expect(hippieDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it("should have a step function that makes its node blink", function() {
+    sinon.spy(hippieDancer.$node, 'toggle');
+    hippieDancer.step();
+    expect(hippieDancer.$node.toggle.called).to.be.true;
+  });
+
+  describe("dance", function(){
+    it("should call step at least once per second", function(){
+      sinon.spy(hippieDancer, "step");
+      expect(hippieDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps);
+      expect(hippieDancer.step.callCount).to.be.equal(1);
+      clock.tick(timeBetweenSteps);
+      expect(hippieDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
